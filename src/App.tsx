10eba560a5151676e66e0af01c931190e54a0b13
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-10-07 21:50:08 -0300

    chore: creating Fomr component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
-
+import Form from './components/Form';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-      </header>
+        <Form/>        
     </div>
   );
 }
