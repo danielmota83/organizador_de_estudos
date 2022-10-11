daaa50b046dd5d501d93425c430fcec72e799ac4
Daniel Mota <101141570+danielmota83@users.noreply.github.com>
2022-10-10 22:21:04 -0300

    refactor

---
+++ src/components/Cronometro/index.tsx
@@ -0,0 +1,17 @@
+import Button from '../Button'
+
+export function Cronometro() {
+
+    return (
+        <div>
+            <p> Escoolha um card e inicie o relógio </p>
+            <div>
+
+            </div>
+            <Button
+                texto = "Começar!"
+            />
+        </div>
+        
+    )
+}
